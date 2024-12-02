69bed76bb90466bdbc1390346048c4f417a968f7
abhishek <abhishekmishra06460@gmail.com>
2024-12-02 15:23:17 +0530

    first create get first

--- controller/blog/getBlogPost.js
+++ controller/blog/getBlogPost.js
@@ -4,7 +4,7 @@ const { sendGeneralResponse } = require("../../utils/responseHelper");
 const getAllBlogs = async (req, res) => {
   try {
     // Fetch all blog posts from the database
-    const blogPosts = await BlogPost.find();
+    const blogPosts = await BlogPost.find().sort({ createdAt: -1 });
 
     // Check if no blog posts exist
     if (!blogPosts.length) {
